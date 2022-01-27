e943febc82eb789dd10f71833875db6b3763aa71
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-01-27 16:44:44 +0800

    Create variables for forms in js file

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// Variables
+const submissionForm = document.getElementById("form--signup");
+const emailInput = document.getElementById("email--signup");
+const submissionForm = document.getElementById("form--offer");
+const emailInput = document.getElementById("email--offer");
